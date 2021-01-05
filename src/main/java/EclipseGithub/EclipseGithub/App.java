53de2ef4790a6dc632ca478f8ad1de3f8103f5ed
Jonathan Nicaud <jonathan.nicaud@hotmail.fr>
2021-01-05 14:48:13 +0100

    Test 1

---
+++ src/main/java/EclipseGithub/EclipseGithub/App.java
@@ -0,0 +1,13 @@
+package EclipseGithub.EclipseGithub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World 1 !" );
+    }
+}
